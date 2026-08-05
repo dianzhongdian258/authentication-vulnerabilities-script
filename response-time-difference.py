c8329b7b246a8dd8539fc66fa2697c131b6a7eb9
dian <368347815@qq.com>
2026-08-05 15:52:15 +0800

    Update response-time-difference.py

--- response-time-difference.py
+++ response-time-difference.py
@@ -13,41 +13,8 @@ VERIFY_TOP = 10
 VERIFY_SAMPLES = 8
 REQUEST_DELAY = 0.2
 
-usernames = [
-    "carlos", "root", "admin", "test", "guest", "info", "adm", "mysql",
-    "user", "administrator", "oracle", "ftp", "pi", "puppet", "ansible",
-    "ec2-user", "vagrant", "azureuser", "academico", "acceso", "access",
-    "accounting", "accounts", "acid", "activestat", "ad", "adam", "adkit",
-    "admin", "administracion", "administrador", "administrator",
-    "administrators", "admins", "ads", "adserver", "adsl", "ae", "af",
-    "affiliate", "affiliates", "afiliados", "ag", "agenda", "agent", "ai",
-    "aix", "ajax", "ak", "akamai", "al", "alabama", "alaska", "albuquerque",
-    "alerts", "alpha", "alterwind", "am", "amarillo", "americas", "an",
-    "anaheim", "analyzer", "announce", "announcements", "antivirus", "ao",
-    "ap", "apache", "apollo", "app", "app01", "app1", "apple", "application",
-    "applications", "apps", "appserver", "aq", "ar", "archie", "arcsight",
-    "argentina", "arizona", "arkansas", "arlington", "as", "as400", "asia",
-    "asterix", "at", "athena", "atlanta", "atlas", "att", "au", "auction",
-    "austin", "auth", "auto", "autodiscover",
-]
-passwords = [
-    "123456", "password", "12345678", "qwerty", "123456789", "12345", "1234",
-    "111111", "1234567", "dragon", "123123", "baseball", "abc123", "football",
-    "monkey", "letmein", "shadow", "master", "666666", "qwertyuiop", "123321",
-    "mustang", "1234567890", "michael", "654321", "superman", "1qaz2wsx",
-    "7777777", "121212", "000000", "qazwsx", "123qwe", "killer", "trustno1",
-    "jordan", "jennifer", "zxcvbnm", "asdfgh", "hunter", "buster", "soccer",
-    "harley", "batman", "andrew", "tigger", "sunshine", "iloveyou", "2000",
-    "charlie", "robert", "thomas", "hockey", "ranger", "daniel", "starwars",
-    "klaster", "112233", "george", "computer", "michelle", "jessica", "pepper",
-    "1111", "zxcvbn", "555555", "11111111", "131313", "freedom", "777777",
-    "pass", "maggie", "159753", "aaaaaa", "ginger", "princess", "joshua",
-    "cheese", "amanda", "summer", "love", "ashley", "nicole", "chelsea",
-    "biteme", "matthew", "access", "yankees", "987654321", "dallas", "austin",
-    "thunder", "taylor", "matrix", "mobilemail", "mom", "monitor", "monitoring",
-    "montana", "moon", "moscow",
-]
-
+usernames = []
+passwords = []
 
 incorrect_password = "A" * 1000
 
@@ -62,8 +29,10 @@ class CsrfInputParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag.lower() != "input":
             return
-        attributes = dict(attrs)  #attrs是列表，表值为二元元组如("type","hidden")，因此可以把表值转换成字典的键值对格式，把列表变成字典以使用.get()方法，但重复的属性-值对会被覆盖
-        if attributes.get("name") == "csrf" and attributes.get("value"):  #.get()方法若没找到对应的键或键值为空，会返回None或空字符串，如果使用attributes[name]，当"name"属性不存在时会抛keyError
+        attributes = dict(attrs) 
+#attrs是列表，表值为二元元组如("type","hidden")，因此可以把表值转换成字典的键值对格式，把列表变成字典以使用.get()方法，但重复的属性-值对会被覆盖
+        if attributes.get("name") == "csrf" and attributes.get("value"):  
+#.get()方法若没找到对应的键或键值为空，会返回None或空字符串，如果使用attributes[name]，当"name"属性不存在时会抛keyError
             self.token = attributes["value"]
 
 
@@ -75,7 +44,7 @@ def validate_lab_url():
 
 def get_csrf_token(session):
     response = session.get(URL, timeout=TIMEOUT, verify=False)
-    response.raise_for_status() #等价于if response.status_code >= 400: #raise HTTPError(f"HTTP错误: {response.status_code}")
+    response.raise_for_status() #等价于if response.status_code >= 400: raise HTTPError(f"HTTP错误: {response.status_code}")
     parser = CsrfInputParser()
     parser.feed(response.text)#拼进内部缓冲区 self.rawdata， 调用 goahead() 主循环开始扫描。扫到<html>标签时执行handle_starttag('html',[]),return扫到<input>标签时执行handle_starttag('input',[("type","hidden"),("name","csrf"),("value","aX9k")])，命中，把self.token设为'aX9k'
     return parser.token
